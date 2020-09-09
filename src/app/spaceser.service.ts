799334a89377a03b73c4ab5f13f4698121e74ee2
1992PrabhatUpadhyay <70603714+1992PrabhatUpadhyay@users.noreply.github.com>
2020-09-09 16:48:28 +0530

    Add files via upload

---
+++ src/app/spaceser.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpaceserService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  public getSpace(){
+    let dataURL='https://api.spaceXdata.com/v3/launches?limit=100'
+    return this.httpClient.get(dataURL)
+
+  }
+  public getSuccess(){
+    let dataURL='https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true'
+    return this.httpClient.get(dataURL)
+  }
+}
